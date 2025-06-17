5aa4ddb8c1613db23fae25b831c90982e70ad47d
asemente <hoshizakizindeveloper07@gmail.com>
2025-06-17 23:08:18 +0100

    admins

--- app.js
+++ app.js
@@ -6,10 +6,22 @@ require('dotenv').config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://arabian-blog.vercel.app'
+];
+
 app.use(cors({
-  origin: 'https://arabian-blog.vercel.app',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Não autorizado por CORS'));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
